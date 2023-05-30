f217b3f56ae4a1375ca66d75a91205e895abe519
kiemtran.dev <kiemtran.dev@gmail.com>
2023-05-30 18:12:01 +0700

    feat: test

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  api: {
+    nxstandalonereactapo: {
+      GraphQLAPIEndpointOutput: 'string';
+      GraphQLAPIIdOutput: 'string';
+      GraphQLAPIKeyOutput: 'string';
+    };
+  };
+};
